ec2e082ec7f2b9ebc43faa665e860e89b84661b3
claymore03 <113389977+claymore03@users.noreply.github.com>
2023-02-09 20:13:39 -0700

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,33 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Prompt user to enter score
         Console.WriteLine("Enter your score here: ");
         string grade = Console.ReadLine();
         int number = int.Parse(grade);
 
+        //Display letter grade based of score
         if (number >= 90)
         {
-            Console.WriteLine("You got an A! Great job.");
+            Console.WriteLine("Grade: A");
         }
         else if (number >= 80 && number < 90)
         {
-            Console.WriteLine("You got a B! Nice.");
+            Console.WriteLine("Grade: B");
         }
         else if (number >= 70 && number < 80)
         {
-            Console.WriteLine("You got a C! You passed the class.");
+            Console.WriteLine("Grade: C");
         }
         else if (number >= 60 && number < 70)
         {
-            Console.WriteLine("You got a D. Better luck next time.");
+            Console.WriteLine("Grade: D");
         }
         else if (number < 70)
         {
-            Console.WriteLine("You got a F. Study harder.");
+            Console.WriteLine("Grade: F");
         }
         else
         {
             Console.WriteLine("INVALID INPUT: Enter a number next time doofus.");
+        }
+
+        //Figure out if PASS or FAIL
+        if(number >= 70)
+        {
+            Console.WriteLine($"You PASSED with a {grade}%");
+        }
+        else if(number < 70)
+        {
+            Console.WriteLine($"You FAILED with a {grade}%");
+        }
+        else
+        {
+            Console.WriteLine("Enter a number fool!");
         }
 
 
