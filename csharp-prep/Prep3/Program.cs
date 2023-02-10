9ca25288c7c8817a991ffd41955b38e67a25b4c7
claymore03 <113389977+claymore03@users.noreply.github.com>
2023-02-09 21:55:23 -0700

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,35 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Prompt user to enter a magic number
-        Console.WriteLine("What is the magic number?");
-        string magic = Console.ReadLine(); 
-        float answer = float.Parse(magic);
+        //Generate a magic number between 1 and 100
+        Console.WriteLine("Welcome to Magic Number! To win, guess the magic number");
 
         //Loops until user "guesses" the magic number
+        float magic_number = 0;
         float guess = 0;
+        string answer = "";
+        string play = "Y";
+        
+        Random randomGenerator = new Random();
+        magic_number = randomGenerator.Next(1, 100);
+
         do
         {
-        Console.WriteLine("Enter a guess: ");
-        string user_input = Console.ReadLine();
-        guess = float.Parse(user_input);
+            Console.WriteLine("Enter a number between 1 and 100: ");
+            string user_input = Console.ReadLine();
+            guess = float.Parse(user_input);
 
-        if(guess < answer)
-        {
-            Console.WriteLine("Higher ^^^");
-        }
-        else if(guess > answer)
-        {
-            Console.WriteLine("Lower vvv");
-        }
-        else if(guess == answer)
-        {
-            Console.WriteLine("You found the magic number!");
-        }
-        else
-        {
-            Console.WriteLine("INVALID INPUT");
-        }
-        } while (guess != answer);
+            if(guess < magic_number)
+            {
+                Console.WriteLine("Higher ^^^");
+            }
+            else if(guess > magic_number)
+            {
+                Console.WriteLine("Lower vvv");
+            }
+            else if(guess == magic_number)
+            {
+                Console.WriteLine("You found the magic number!");
+                do {
+                Console.WriteLine("Continue playing? (Y or N): ");
+                answer = Console.ReadLine();
+                play = answer.ToUpper();
+                } while (play != "Y" && play != "N");
+                magic_number = randomGenerator.Next(1, 100);
+            }
+            else
+            {
+                Console.WriteLine("INVALID INPUT");
+            }
+
+        } while (guess != magic_number && play == "Y");
     }
 }
