6a63645c6b900562abd29d3ce6c4dcaa5f608da8
claymore03 <113389977+claymore03@users.noreply.github.com>
2023-02-09 19:35:49 -0700

    Completed Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Enter your first name: ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name: ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"The name is {last_name}, {first_name} {last_name}.");
     }
 }
