912b2a54914422e7a8fc42de6190e7537ffdfb08
claymore03 <113389977+claymore03@users.noreply.github.com>
2023-02-09 20:53:06 -0700

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Prompt user to enter a magic number
+        Console.WriteLine("What is the magic number?");
+        string magic = Console.ReadLine(); 
+        float answer = float.Parse(magic);
+
+        //Loops until user "guesses" the magic number
+        float guess = 0;
+        do
+        {
+        Console.WriteLine("Enter a guess: ");
+        string user_input = Console.ReadLine();
+        guess = float.Parse(user_input);
+
+        if(guess < answer)
+        {
+            Console.WriteLine("Higher ^^^");
+        }
+        else if(guess > answer)
+        {
+            Console.WriteLine("Lower vvv");
+        }
+        else if(guess == answer)
+        {
+            Console.WriteLine("You found the magic number!");
+        }
+        else
+        {
+            Console.WriteLine("INVALID INPUT");
+        }
+        } while (guess != answer);
     }
 }
