7016cba1a1dffd237b82654710ef33fe8796c023
claymore03 <113389977+claymore03@users.noreply.github.com>
2023-02-21 22:00:39 -0700

    Update Project W03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,12 +1,39 @@
 using System;
-
+//Clayton Redding
+//02-21-2023
+//Brother Comeau
+//CSE210
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-
+        //First instance of Job Class
         Job job1 = new Job();
+        job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job1.DisplayJobDetails();
+
+        Console.WriteLine();
+
+        //Second instance of Job class
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Data Analyst";
+        job2._startYear = 2014;
+        job2._endYear = 2018;
+        job2.DisplayJobDetails();
+
+        Console.WriteLine();
+
+        //Instance of Resume
+        Resume resume1 = new Resume();
+        resume1._name = "Clayton Redding";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
+
     }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,9 +1,22 @@
-using System;
-
-class Resume
+public class Resume
 {
-    static void Main(string[] args)
+   public string _name = "";
+
+   public List<Job> _jobs = new List<Job>();
+
+   public Resume()
+   {
+
+   }
+
+   public void DisplayResume()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        // Console.WriteLine($"{_jobs}");
+        for(int i=0;i<_jobs.Count;i++)
+            {
+                _jobs[i].DisplayJobDetails();
+            }
     }
 }
