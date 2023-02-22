3c678f2c4a8d502d26bdcb0a7c9ccc4507580699
claymore03 <113389977+claymore03@users.noreply.github.com>
2023-02-21 20:13:09 -0700

    Adding Jobs to the repository

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+public class Job
+{
+    // public string _company = "";
+    public string _jobTitle = "";
+    public Job()
+    {
+
+    }
+    // public int _startYear = 0;
+    // public int _endYear = 0;
+}
